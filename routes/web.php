46fc03c0dbbcb32d072f2607547e6480c81fe447
Nasreen <nasreenaqmal12@gmail.com>
2025-10-04 18:19:47 +0800

    - Add dashboard page route

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,11 @@ use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SessionController;
 
+Route::view('/', 'dashboard');
+Route::view('/dashboard', 'dashboard');
+
 Route::middleware('guest')->group(function () {
     Route::get('/register', [RegisteredUserController::class, 'create']);
 
     Route::get('/login', [SessionController::class, 'create'])->name('login');
-}); 
+});
