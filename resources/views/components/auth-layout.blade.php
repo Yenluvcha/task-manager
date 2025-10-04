c1bffe98eba0d09f505f44c1ee2b5f8f4e6f6876
Nasreen <nasreenaqmal12@gmail.com>
2025-10-04 18:19:28 +0800

    - Add dark mode

--- resources/views/components/auth-layout.blade.php
+++ resources/views/components/auth-layout.blade.php
@@ -7,6 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     @vite(['resources/js/app.js', 'resources/css/app.css'])
+    <script>
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark')
+        } 
+    </script>
 </head>
 
 <body class="h-full font-inter">
@@ -14,8 +21,10 @@
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
 
         {{ $slot }}
-        
+
     </div>
+
+    <script src="{{ Vite::asset('resources/js/layout.js') }}"></script>
 
 </body>
 
